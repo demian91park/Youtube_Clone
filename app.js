4e22d4b70468f4acc5d63bc9bd36dfe6ebc0a370
demian91park <38171288+demian91park@users.noreply.github.com>
2020-01-29 15:54:56 +0900

    make controller and routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bodyparser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
 
@@ -18,8 +19,8 @@ app.use(bodyparser.json());
 app.use(helmet());
 
 app.use(morgan("dev"));
-app.use("/", globalRouter);
-app.use("/user",userRouter);
-app.use("/video",videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
 
-
+userRouter.get(routes.users, (req, res) => res.send("users"));
+userRouter.get(routes.userDetail, (req, res) => res.send('userDetail'));
+userRouter.get(routes.editProfile, (req, res) => res.send('edit profile'));
+userRouter.get(routes.changePassword, (req,res) => send.send('change password'));
 export default userRouter;
