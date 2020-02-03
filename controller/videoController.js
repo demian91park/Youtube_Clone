9035992cec52a27482c9b475bf4b83e296718b7c
demian91park <38171288+demian91park@users.noreply.github.com>
2020-02-03 17:05:37 +0900

    search work

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,6 +1,11 @@
 export const home = (req, res ) => res.render("home",{ pageTitle: "Home", potato: 1234});
-export const search = (req,res) => res.render("search", { pageTitle: "search"});
-
+export const search = (req, res) => {
+    const {
+      query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+  };
+  
 export const upload = (req,res) => res.render("upload",{ pageTitle: "upload"});
 export const videoDetail = (req, res) => res.render("VideoDetail",{ pageTitle: "videoDetail"});
 export const editVideo = (req, res) => res.render("editVideo",{ pageTitle: "editVideo"});
